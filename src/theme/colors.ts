062fd632b81a8e04778a618a20aa103a61760633
Josue-Santos-Pinto <js.pinto55@gmail.com>
2023-11-24 14:36:45 -0300

    first commit

---
+++ src/theme/colors.ts
@@ -0,0 +1,10 @@
+export const colors = {
+  primary_800: '#00E0FF',
+  primary_700: '#74F9FF',
+  primary_200: '#A6FFF2',
+  primary_100: '#E8FFE8',
+
+  white: '#FFF',
+  black: '#000',
+  gray: '#999',
+};
